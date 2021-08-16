4a2d45e0316f3adeda90760be4c1064a806d859d
branudeep2020 <branudeep2020@gmail.com>
2021-08-16 10:35:03 +0530

    init_1

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const colors = require('colors');
+const cors = require('cors');
+const conn = require('./dbcon');
+const config = require('./config');
+const { forwardAuthenticated } = require('./sessionValidator');
 
+const app = express(); 
+const PORT = process.env.PORT || 3002;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(session({
+    secret : config.sessionSecrect,
+    resave : true,
+    saveUninitialized : true,
+}));
+
+app.get('/',forwardAuthenticated, (req,res)=>{
+    //res.sendFile(path.join(__dirname + '/index.html'));
+    res.render('pages/index');
+});
+
+const server = app.listen(PORT, '0.0.0.0', ()=>{
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('SUCCESS : SERVER STARTED LISTENING AT : http://%s:%s'.green, host, PORT);
+});
